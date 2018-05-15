2bdc377c6556e413b6a5afb347ba7b0707ba9129
romeyer <digao.meyer@gmail.com>
2018-05-15 12:02:16 -0300

    Switch case

---
+++ switch-case/exemplo.php
@@ -0,0 +1,38 @@
+<?php
+	
+	$diaDaSemana = date("w");
+
+	switch ($diaDaSemana) {
+		
+		case 1:
+		echo "Domingo";
+		break;
+
+		case 2:
+		echo "segunda-feira";
+		break;
+
+		case 3:
+		echo "terça-feira";
+		break;
+
+		case 4:
+		echo "quarta-feira";
+		break;
+
+		case 5:
+		echo "quinta-feira";
+		break;
+
+		case 6:
+		echo "sexta-feira";
+		break;
+
+		case 7:
+		echo "sábado";
+		break;
+
+	}
+
+?>
+
